bfdbefd0a28581bf8112d6bf1f304b476071b396
Darlene Matos <darlenem19@gmail.com>
2022-08-09 23:43:20 -0400

    Add new API call

---
+++ group-project/src/constants/sunrise-api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+
+// export async function SunInfo (lat , lng) {
+//     const sunInfoResponse = await axios.get(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`)
+//     console.log(sunInfoResponse, 'api call')
+//     return sunInfoResponse
+// }
--- group-project/src/pages/Home.js
+++ group-project/src/pages/Home.js
@@ -1,4 +1,6 @@
 import { Location } from "../constants/location-api";
+// import { SunInfo } from "../constants/sunrise-api";
+import { Info } from "../constants/otherapi";
 import Search from "../components/Search";
 import { useState } from "react";
 
@@ -9,29 +11,48 @@ const [latitude, setLatitude] = useState('');
 const [longitude, setLongitude] = useState('');
 
 
-async function getLocation () {
+// async function getLocation () {
     
-    console.log(city)
-   try {   
+//     console.log(city)
+//    try {   
 
-    let response = await Location(city)
-    let results = response.data.results[0].geometry.location
-    console.log(results)
-    setLatitude(results.lat)
-    setLongitude(results.lng)
+//     let response = await Location(city)
+//     let results = response.data.results[0].geometry.location
+//     console.log(results)
+//     setLatitude(results.lat)
+//     setLongitude(results.lng)
 
-   }catch(error) {
-    console.log(error)
-   }          
+//    }catch(error) {
+//     console.log(error)
+//    }          
            
+// }
+
+async function getSunriseInfo () {
+
+    let results = await Info(city)
+    console.log(results)
+    // let sunResults = await SunInfo(latitude, longitude)
+    // console.log(sunResults.data.results.sunrise)
 }
+
+async function getSunsetInfo () {
+    let results = await Info(city)
+    console.log(results)
+    // let sunResults = await SunInfo(latitude, longitude)
+    // console.log(sunResults.data.results.sunset)
+}
+
     return (
         <div>
             < Search city={city} setCity ={setCity} />
-            <button onClick= {getLocation}> Search </button>  
+            <button> Search </button>  
             City: {city}
             Latitude: {latitude}
             Longitude: {longitude}
+            <button onClick={getSunriseInfo}> SUNRISE </button>
+            <button onClick={getSunsetInfo}> SUNSET </button>
+            <button> MOONPHASE </button> 
         </div>
     )
 }
