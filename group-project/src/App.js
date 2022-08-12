438ecf8aa49ac2dbea08f13e8f9555018582cafc
Haeyoung Lee <heyonlee850@gmail.com>
2022-08-11 20:37:06 -0400

    modified Header.js

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,12 +1,24 @@
-import Home from './pages/Home';
+
+import Header from './pages/Header';
 import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     <Home />  
+     <div className="App">
+     <Header/>
+     {/* <Home />   */}
+     
+
     </div>
+    
+  
+
   );
+
+  
 }
 
 export default App;
