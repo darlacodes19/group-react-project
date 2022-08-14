edd5133adf9e96385ac35c081afdbb668daf226a
Darlene Matos <darlenem19@gmail.com>
2022-08-14 00:43:56 -0400

    Add background color change on button click

--- group-project/src/components/Buttons.js
+++ group-project/src/components/Buttons.js
@@ -1,9 +1,10 @@
 
 
 function Buttons (props) {
+   
 
     return (
-        <div> 
+        <div className={props.style}> 
             <button onClick={props.sunriseButton}> SUNRISE </button>
             <button onClick={props.sunsetButton}> SUNSET </button>
             <button onClick={props.moonphaseButton}> MOONPHASE </button> 
